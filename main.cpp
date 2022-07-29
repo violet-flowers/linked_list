dc2834ada13df175a737643d79e7fe2047cc291f
violet-flowers <109183645+violet-flowers@users.noreply.github.com>
2022-07-29 14:52:55 +0100

    Added Input Files/Updated Client Side Code to support char+int input.

--- main.cpp
+++ main.cpp
@@ -6,27 +6,41 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    bool isDigit = false;
 
     if (argc != 2) {
         cout << "Usage" << argv[0] << "name of Input File" << endl;
     }
 
-    auto linkedList = new LList<int>;
+    auto linkedList_int = new LList<int>;
+    auto linkedList_char = new LList<char>;
     ifstream listStream;
     listStream.open(argv[1],ios::in);
-    int data;
+    char data_char;
 
-    while (listStream >> data){
-        if(isdigit(data)){
-            linkedList->append(data-'0');
-        }else {
-            linkedList -> append(data);
+    while (listStream >> data_char) {
+        if(isdigit(data_char)) {
+            isDigit = true;
+            linkedList_int->append(data_char - '0');
+        }
+        else {
+            linkedList_char->append(data_char);
         }
     }
 
-    linkedList -> print();
+    if(isDigit){
+        string palindrome = linkedList_int->isPalindrome() ? "\nIs" : "\nIs not";
+        linkedList_int->print();
+        cout<<palindrome<<" a palindrome."<<endl;
+    }else {
+        string palindrome = linkedList_char->isPalindrome() ? "\nIs" : "\nIs not";
+        linkedList_char->print();
+        cout<<palindrome<<" a palindrome."<<endl;
+    }
 
-    cout<<"\nIs Palindrome: "<<linkedList->isPalindrome()<<endl;
 
+
+
+    listStream.close();
     return 0;
 }
